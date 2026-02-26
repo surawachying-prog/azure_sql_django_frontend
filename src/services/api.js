18e5ff9c2c52b35927056f09c771b87344ade3e3
Surawach Yingpunyachok <surawach.ying@mail.kmutt.ac.th>
2026-02-27 00:50:55 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -61,7 +61,7 @@ export const reviewService = {
 };
 
 // Function App URL - Replace with your actual Azure Function App URL
-const FUNCTION_API_URL = 'https://bearlab-function-app-baebfye2fnb7bmcj.southeastasia-01.azurewebsites.net/api';
+const FUNCTION_API_URL = 'https://surawach-filter-app-cnavgbfdaaghcxgm.southeastasia-01.azurewebsites.net/api';
 
 const functionApi = axios.create({
   baseURL: FUNCTION_API_URL,
